eacfc082ff7ecdad77bb19d65c1666ac945869d1
ralamazel <maxime.bertrou@gmail.com>
2019-11-29 14:27:24 +0100

    fix #3 Tests sur ScoreCalculateur

---
+++ src/test/java/miagem1/ScoreCalculateurTest.java
@@ -0,0 +1,48 @@
+package miagem1;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class ScoreCalculateurTest {
+
+    QuestionAChoixMultiple questionAChoixMultiple ;
+    ScoreCalculateur scoreCalculateur;
+
+    @Before
+    public void setUp() throws Exception {
+        // given
+        this.questionAChoixMultiple = new QuestionAChoixMultiple("q1",new ArrayList<Integer>(Arrays.asList(2,3,5)));
+        this.scoreCalculateur = new ScoreCalculateur();
+
+
+    }
+
+    @Test
+    public void testCalculeScoreWhenReponseIsBad() {
+        ArrayList<Integer> listIndice = new ArrayList<Integer>(Arrays.asList(1,4));
+        float score = scoreCalculateur.calculeScore(listIndice, this.questionAChoixMultiple);
+
+        assertEquals(0f, score ,0.00f);
+    }
+
+    @Test
+    public void testCalculeScoreWhenReponseIsGood() {
+        ArrayList<Integer> listIndice = new ArrayList<Integer>(Arrays.asList(2,3,5));
+        float score = scoreCalculateur.calculeScore(listIndice, this.questionAChoixMultiple);
+
+        assertEquals(100f, score ,0.01f);
+    }
+
+    @Test
+    public void testCalculeScoreWhenReponseIsGoodAndBad() {
+        ArrayList<Integer> listIndice = new ArrayList<Integer>(Arrays.asList(2,3));
+        float score = scoreCalculateur.calculeScore(listIndice, this.questionAChoixMultiple);
+        float scoreAttendu = 200f/3;
+        assertEquals(scoreAttendu, score ,0.01f);
+    }
+}
