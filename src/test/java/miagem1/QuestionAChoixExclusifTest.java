ff1a567447d98d364f9b0d1b341c4fe3bbbb22ed
ralamazel <maxime.bertrou@gmail.com>
2019-11-29 14:02:13 +0100

    fix #2 Tests sur QuestionAChoixMultiple

--- src/test/java/miagem1/QuestionAChoixExclusifTest.java
+++ src/test/java/miagem1/QuestionAChoixExclusifTest.java
@@ -1,4 +1,41 @@
 package miagem1;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
 public class QuestionAChoixExclusifTest {
+
+    private QuestionAChoixExclusif question;
+
+    @Before
+    public void setUp() throws Exception {
+        // given
+        question =  new QuestionAChoixExclusif("enonce",2);
+    }
+
+    @Test
+    public void testGetEnonce() {
+        String enonce = question.getEnonce();
+        assertEquals("enonce", enonce);
+
+    }
+
+    @Test
+    public void testGetScoreForIndiceBonneReponse() {
+        int indiceEtudiant = 2;
+        float resultat = question.getScoreForIndice(indiceEtudiant);
+        // then : le score obtenu est 100
+        assertEquals(100f ,resultat,0.01f);
+    }
+
+    @Test
+    public void testGetScoreForIndiceMauvaiseReponse() {
+        int indiceEtudiant = 1;
+        float resultat = question.getScoreForIndice(indiceEtudiant);
+        // then : le score obtenu est 100
+        assertEquals(0f ,resultat,0.01f);
+    }
 }
--- src/test/java/miagem1/QuestionAChoixMultipleTest.java
+++ src/test/java/miagem1/QuestionAChoixMultipleTest.java
@@ -3,19 +3,42 @@ package miagem1;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class QuestionAChoixMultipleTest {
 
+    private QuestionAChoixMultiple question;
+
     @Before
     public void setUp() throws Exception {
+        // given
+        ArrayList<Integer> listIndice = new ArrayList<Integer>();
+        listIndice.add(1);
+        listIndice.add(2);
+        question =  new QuestionAChoixMultiple("enonce",listIndice);
     }
 
     @Test
     public void getEnonce() {
+        String enonce = question.getEnonce();
+        assertEquals("enonce",enonce);
     }
 
     @Test
-    public void getScoreForIndice() {
+    public void getScoreForIndiceBonneReponse() {
+        int indiceEtudiant = 1;
+        float resultat = question.getScoreForIndice(indiceEtudiant);
+        assertEquals(50f ,resultat,0.01f);
     }
+
+    @Test
+    public void getScoreForIndiceMauvaiseReponse() {
+        int indiceEtudiant = 3;
+        float resultat = question.getScoreForIndice(indiceEtudiant);
+        assertEquals(0f ,resultat,0.01f);
+    }
+
 }
